abdf983d4938df89c9c2c7751cf461fa0ee9a57d
陈益方 <705819321@163.com>
2020-12-01 15:41:00 +0800

    普通版 服务提供者 服务使用者 测试完成

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName PaymentMain8001
+ * @Description TODO
+ * @Author 陈益方
+ * @Date 2020/12/1
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @ClassName CommonResult
+ * @Description TODO
+ * @Author 陈益方
+ * @Date 2020/12/1
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommonResult<T> {
+
+    private Integer code;
+
+    private String message;
+
+    private T data;
+
+    public CommonResult(Integer code, String message){
+        this(code,message,null);
+    }
+}
