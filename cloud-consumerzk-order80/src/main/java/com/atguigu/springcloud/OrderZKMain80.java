d6811f04d151bee45ce9c611047f16308f820596
陈益方 <705819321@163.com>
2020-12-07 15:28:47 +0800

    zookeeper 服务使用者

---
+++ cloud-consumerzk-order80/src/main/java/com/atguigu/springcloud/OrderZKMain80.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ClassName OrderZKMain80
+ * @Description TODO
+ * @Author 陈益方
+ * @Date 2020/12/7
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderZKMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZKMain80.class, args);
+    }
+}
